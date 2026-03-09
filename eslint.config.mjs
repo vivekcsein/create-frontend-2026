f4b66976de967a68c17c6883b08eeab3017e4405
vivekcsein <vivekcse.in@gmail.com>
2026-03-09 20:08:02 +0530

    test: husky working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
-import { defineConfig, globalIgnores } from "eslint/config"
-import nextVitals from "eslint-config-next/core-web-vitals"
-import nextTs from "eslint-config-next/typescript"
-import prettier from "eslint-config-prettier"
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-])
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main></main>
+  return <main>Next.js begginer template...</main>;
 }
